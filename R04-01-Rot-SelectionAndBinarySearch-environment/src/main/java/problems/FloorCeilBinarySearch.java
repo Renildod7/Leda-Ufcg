f85c4f1e09ce6f7ae9cf2b93a82f0155cf56283f
Renildod7 <renildo.melo@ccc.ufcg.edu.br>
2019-09-05 16:55:48 -0300

    R04 - FlooCeilBinarySearch implementado (pode ter bug ainda).

--- R04-01-Rot-SelectionAndBinarySearch-environment/src/main/java/problems/FloorCeilBinarySearch.java
+++ R04-01-Rot-SelectionAndBinarySearch-environment/src/main/java/problems/FloorCeilBinarySearch.java
@@ -15,9 +15,65 @@ public class FloorCeilBinarySearch implements FloorCeil {
 
 	@Override
 	public Integer floor(Integer[] array, Integer x) {
-		// TODO implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return floor(array, x, 0, array.length-1);
 	}
+
+	private Integer floor(Integer[] array, Integer x, int leftIndex, int rightIndex) {
+		
+		if(leftIndex < rightIndex) {
+			
+			int meio = (leftIndex + rightIndex) / 2;
+			
+			if(array[meio] == x) {
+				return array[meio-1];
+				
+			}else if(array[meio] < x) {
+				return floor(array, x, meio + 1, rightIndex, array[meio]);
+				
+			}else {
+				return floor(array, x, leftIndex, meio -1);
+			}
+		
+		} else {
+			return null;
+		}
+		
+	}
+
+	private Integer floor(Integer[] array, Integer x, int leftIndex, int rightIndex, int menorAtual) {
+		
+		
+		if(leftIndex == rightIndex) {
+			
+			if(array[leftIndex] < x) {
+				return array[leftIndex];
+			
+			}else {
+				return menorAtual;
+			}
+		}
+		
+		
+		
+			
+		int meio = (leftIndex + rightIndex) / 2;
+		
+		if(array[meio] == x) {
+			return array[meio-1];
+			
+		}else if(array[meio] < x) {
+			return floor(array, x, meio + 1, rightIndex, array[meio]);
+		
+		}else {
+			return floor(array, x, leftIndex, meio-1, menorAtual);
+		}
+			
+
+	}
+	
+	
+	
+	
 
 	@Override
 	public Integer ceil(Integer[] array, Integer x) {
